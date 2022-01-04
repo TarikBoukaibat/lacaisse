b0ed14bc05a6740154eb33856e8cdd8e2778d672
tarikbouk <96335226+tarikbouk@users.noreply.github.com>
2022-01-05 00:22:59 +0100

    Update ManagerDaoImpl.java

--- src/main/java/com/application/Dao/Implementation/ManagerDaoImpl.java
+++ src/main/java/com/application/Dao/Implementation/ManagerDaoImpl.java
@@ -1,5 +1,10 @@
 package com.application.Dao.Implementation;
 
 public class ManagerDaoImpl {
+  public static  void save(Employee employee){
 
+        String query= "inset into employee ( Firstname, Lastname, Username, Gender,Salary,email, Role) values('"+employee.getFirstname()+"','"+employee.getLastname()+"','"+employee.getUsername()+"','"+employee.getEmail()+"','"+employee.getSalary()+"','"+employee.getGender()+"','"+employee.getRole()+"','"+employee.getPassword()+"')";
+        DbOperations.SetData(query);
+    }
+  
 }
