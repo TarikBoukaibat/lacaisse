b8a2b4766f7629d31eec9a6c66a225d2aac7c5eb
tarikbouk <96335226+tarikbouk@users.noreply.github.com>
2022-01-05 00:28:47 +0100

    Update ManagerDaoImpl.java

--- src/main/java/com/application/Dao/Implementation/ManagerDaoImpl.java
+++ src/main/java/com/application/Dao/Implementation/ManagerDaoImpl.java
@@ -1,9 +1,9 @@
 package com.application.Dao.Implementation;
 
 public class ManagerDaoImpl {
-  public static  void save(Employee employee){
+  public static  void saveEmp(Employee employee){
 
-        String query= "inset into employee ( Firstname, Lastname, Username, Gender,Salary,email, Role) values('"+employee.getFirstname()+"','"+employee.getLastname()+"','"+employee.getUsername()+"','"+employee.getEmail()+"','"+employee.getSalary()+"','"+employee.getGender()+"','"+employee.getRole()+"','"+employee.getPassword()+"')";
+        String query= "insert into employee ( Firstname, Lastname, Username, Gender,Salary,email, Role,Password) values('"+employee.getFirstname()+"','"+employee.getLastname()+"','"+employee.getUsername()+"','"+employee.getEmail()+"','"+employee.getSalary()+"','"+employee.getGender()+"','"+employee.getRole()+"','"+employee.getPassword()+"')";
         DbOperations.SetData(query);
     }
   
